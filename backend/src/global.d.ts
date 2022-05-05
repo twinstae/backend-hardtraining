f996ae3fb27ee7efa24818e60ff31cac50f244e1
Taehee Kim <taehee.kim@sap.com>
2022-05-05 10:50:53 +0900

    인터페이스 타입 정리

--- backend/src/global.d.ts
+++ backend/src/global.d.ts
@@ -19,14 +19,14 @@ type Todo = {
 // 부수효과가 있는 function (=procedure) => 반환하는 값이 없어요! void
 // 의존성이 있는 function () => 외부 상태에 따라서 반환하는 값이 늘 변해요!
 
-interface TodoRepository {
+interface ITodoRepository {
   saveAll: (todoList: Todo[]) => Promise<void>; // todoList를 넘기면 어떤 방식인지는 모르지만 저장을 한다 (create or update) // 부수효과
   //saveAll이 리턴값에 따라 getAll이 리턴하는 값이 달라짐 read = 의존성이 있는 함수
   getAll: () => Promise<Todo[]>// todoList 전체를 리턴;   // 아무 것도 안 넘기면... 어떤 방식인지는 모르지만 todoList를 준다 (read) // 의존성
 }
 
 // implements
-interface TodoListService {
+interface ITodoListService {
   // mutation, command
   addTodo: (newTodo: Todo) => Promise<void>
   deleteTodo: (targetContent: string) => Promise<void>
