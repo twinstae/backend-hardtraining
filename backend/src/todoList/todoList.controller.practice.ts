3b3a71bd3b9dec025cb040338382db834d65229a
chloeeee210 <chloeseo0210@gmail.com>
2022-05-18 18:00:32 +0900

    add : ITodoListRepository, ITodoListService 구현, TodoListController 구현 마무리

--- backend/src/todoList/todoList.controller.practice.ts
+++ backend/src/todoList/todoList.controller.practice.ts
@@ -1,29 +1,39 @@
 //import { TodoListService } from "./todoList.service";
-import { Controller, Get, Post, Patch, Delete, Body, Param } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Delete,
+  Body,
+  Param,
+  Inject
+} from "@nestjs/common";
 
 @Controller()
 export class TodoListController {
-  constructor(private readonly todoListService: ITodoListService) {}
-  // get은 await가 없어요~왜요?
+  constructor(
+    @Inject("TODO_SERVICE") //@injectable과의 차이?
+    private readonly todoListService: ITodoListService
+  ) {}
+
   @Get("/todo-list")
   async getTodoList(): Promise<Todo[]> {
     return this.todoListService.getTodoList();
   }
-  // [핑의 오개념~]url에 path-variable안달아줘도됨~!
+
   @Post("/todo-list")
   async addTodo(@Body() newTodo: Todo) {
     await this.todoListService.addTodo(newTodo);
   }
 
-  @Patch("/todo-list:targetContent")
-  async completeTodo(@Param('targetContent') targetContent : string) {
+  @Patch("/todo-list/:targetContent")
+  async completeTodo(@Param("targetContent") targetContent: string) {
     await this.todoListService.completeTodo(targetContent);
   }
 
-  @Delete("/todo-list:targetContent")
-  async deleteTodo(
-      @Param('targetContent') targetContent : string
-  ){
+  @Delete("/todo-list/:targetContent")
+  async deleteTodo(@Param("targetContent") targetContent: string) {
     await this.todoListService.deleteTodo(targetContent);
   }
 }
