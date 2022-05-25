8248105bedc3ee205250cad3b7649f3d062eeba2
Taehee Kim <taehee.kim@sap.com>
2022-05-25 10:05:43 +0900

    feat: react-hook-form 으로 migration, error message
    Co-authored-by: MIMIKK00 <MIMIKK00@users.noreply.github.com>
    Co-authored-by: antartic-penguin <chloeeee210@users.noreply.github.com>
    Co-authored-by: PERFUME <perfumelim@users.noreply.github.com>

--- realworld-next/pages/auth/signup.tsx
+++ realworld-next/pages/auth/signup.tsx
@@ -1,13 +1,29 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
+import { useForm } from "react-hook-form";
 
 const SignUp: NextPage = () => {
-  // username
-  // email
-  // password
+  const { register, formState: { errors }, handleSubmit } = useForm();
 
-
-
+  // // 서버에 요청 보내기
+  // const API_ROOT = 'https://api.realworld.io/api';         
+  
+  // fetch(API_ROOT+'/users', {
+  //   method: 'POST',
+  //   headers: {
+  //     'Content-Type': 'application/json',
+  //   },
+  //   body: JSON.stringify({
+  //     user: Object.fromEntries(data.entries())
+  //   }),
+  // }).then(res => {
+  //   console.log(res.status, res.statusText)
+  //   return res.json()
+  // }).then(body => {
+  //   console.log(body)
+  // })
+  console.log('render!')
+  console.log(errors)
   return (
     <div>
       <Link href="/">
@@ -18,38 +34,37 @@ const SignUp: NextPage = () => {
       <main>
         <h1>Sign Up</h1>
         <form
-          id="signup-form"
-          onSubmit={(e) => {
-          e.preventDefault()
-
-          const data = new FormData(document.getElementById('signup-form') as HTMLFormElement)
-
-          // 서버에 요청 보내기
-          const API_ROOT = 'https://api.realworld.io/api';         
-          
-          fetch(API_ROOT+'/users', {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              user: Object.fromEntries(data.entries())
-            }),
-          }).then(res => {
-            console.log(res.status, res.statusText)
-            return res.json()
-          }).then(body => {
-            console.log(body)
-          })
-        }}>
+          onSubmit={handleSubmit((data) => alert(JSON.stringify(data)))}>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" required minLength={8} />
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" required />
-        <label htmlFor="password">Password</label>
-        <input type="password" id="password" name="password" required minLength={8}
-          pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$" />
+        <input {...register("username", {
+          required: true,
+          minLength: 8,
+        })} type="text" id="username"/>
+        {errors.username?.type === 'required' && "username is required"}
 
+        <br />
+        <label htmlFor="email">Email</label>
+        <input {...register("email", {
+          required: true,
+        })}  type="email" id="email" />
+        {errors.email?.type === 'required' && "email is required"}
+
+        <br />
+        <label htmlFor="password">Password</label>
+        <input
+         type="password" id="password"
+          {...register("password", {
+            required: true,
+            minLength: 8,
+            validate: {
+              includesAlphabet: v => /[a-zA-Z]/.test(v) || '알파벳을 포함해야 합니다.',
+              includesNumber: v => /[0-9]/.test(v) || '숫자를 포함해야 합니다',
+              includesSpecialCharacters: v => /[!?@#$%^&*():;+\-=~{}<>]/.test(v) || '특수문자를 포함해야 합니다',
+            }
+          })}
+         />
+        {errors.password?.type === 'required' && "password is required"}
+        {errors.password?.type !== 'required' && errors.password?.message}
         <button type="submit">Submit</button>
       </form>
       </main>
