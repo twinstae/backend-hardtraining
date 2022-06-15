79598dc675d78802403e0ed8ed869dc531f64f3c
chloeseo0210 <chloeseo0210@gmail.com>
2022-06-15 21:17:13 +0900

    add : fsRepository 구현

--- backend/src/fsRepository.practice.ts
+++ backend/src/fsRepository.practice.ts
@@ -1,20 +1,19 @@
-// import * as fs from "fs/promises";
-// const FILE_NAME = "todolist.json";
+import * as fs from "fs/promises";
+const FILE_NAME = "todolist.json";
 
-// const fsRepository : TodoRepository = {
-//   // 함수
-//   async saveAll(todoList) {
-//     const data = JSON.stringify(todoList);
-//     await fs.writeFile("./" + FILE_NAME, data, { encoding: "utf8" });
-//   },
+const fsRepository : ITodoRepository = {
+    async saveAll(todoList){
+        const data = JSON.stringify(todoList)
+        fs.writeFile(FILE_NAME, data, {encoding : "utf-8"})
+    },
+    async getAll() {
+        const todoList = await fs.readFile(FILE_NAME, {encoding:"utf-8"})
+        const data = JSON.parse(todoList)
+    
+        return data;
+    }
 
-//   async getAll() {
-//     //readFile(path: PathLike | fs.FileHandle,
-//     //options?: { encoding?: null; flag?: OpenMode; } & EventEmitter.Abortable): Promise<Buffer>
-//     const json = await fs.readFile("./" + FILE_NAME, { encoding: "utf-8" });
-//     // type casting?
-//     return JSON.parse(json) as Todo[];
-//   },
-// };
 
-// export default fsRepository;
+}
+
+export default fsRepository;
